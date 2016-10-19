ec6d99c921847bf0d38663a4f16f214c6a754996
lizhou <lizhou@efun.com>
2016-10-19 16:37:59 +0800

    init

---
+++ src/main/java/com/efun/mainland/util/core/Identifiable.java
@@ -0,0 +1,16 @@
+package com.efun.mainland.util.core;
+
+/**
+ * 权限验证接口
+ * 
+ * @author Administrator
+ *
+ */
+public interface Identifiable {
+	/**
+	 * 验证
+	 * 
+	 * @return
+	 */
+    String verificate();
+}
