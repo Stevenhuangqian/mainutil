1d2b4de1b04fe25b53b4a6583ca3925be0bd2157
lizhou <lizhou@efun.com>
2016-11-18 09:52:21 +0800

    添加emoji相关方法

--- src/main/java/com/efun/mainland/util/CommonUtil.java
+++ src/main/java/com/efun/mainland/util/CommonUtil.java
@@ -1085,10 +1085,77 @@ public class CommonUtil {
         }
     }
 
-    public static final void closeResource(Closeable resource){
+    public static final void closeResource(Closeable resource) {
         try {
             resource.close();
-        }catch (Exception e){
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * 判别是否包含Emoji表情
+     *
+     * @param str
+     * @return
+     */
+    public static final boolean containsEmoji(String str) {
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (isEmojiCharacter(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判别是否是Emoji字符
+     *
+     * @param codePoint
+     * @return
+     */
+    public static final boolean isEmojiCharacter(char codePoint) {
+        return !((codePoint == 0x0) ||
+                (codePoint == 0x9) ||
+                (codePoint == 0xA) ||
+                (codePoint == 0xD) ||
+                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF)));
+    }
+
+    /**
+     * 过滤Emoji字符
+     *
+     * @param source
+     * @return
+     */
+    public static final String filterEmoji(String source) {
+        if (!containsEmoji(source)) {
+            return source;// 如果不包含，直接返回
+        }
+
+        StringBuilder buf = null;
+        int len = source.length();
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+            if (!isEmojiCharacter(codePoint)) {
+                if (buf == null) {
+                    buf = new StringBuilder(source.length());
+                }
+                buf.append(codePoint);
+            } else {
+            }
+        }
+        if (buf == null) {
+            return "";
+        } else {
+            if (buf.length() == len) {// 这里的意义在于尽可能少的toString，因为会重新生成字符串
+                buf = null;
+                return source;
+            } else {
+                return buf.toString();
+            }
         }
     }
 
