b56937533d47aa0d816edfd2b30479adca349ad3
lizhou <lizhou@efun.com>
2016-12-20 14:16:04 +0800

    update

--- src/main/java/com/efun/mainland/util/PropertiesEncryptFactoryBean.java
+++ src/main/java/com/efun/mainland/util/PropertiesEncryptFactoryBean.java
@@ -4,34 +4,37 @@ import org.springframework.beans.factory.FactoryBean;
 
 public class PropertiesEncryptFactoryBean implements FactoryBean<Object> {
 
-	private Object property;
+    private Object property;
 
-	public Object getObject() throws Exception {
-		return getProperty();
-	}
+    public Object getObject() throws Exception {
+        return getProperty();
+    }
 
-	public Class<Object> getObjectType() {
-		return Object.class;
-	}
+    public Class<?> getObjectType() {
+        if (property != null)
+            return property.getClass();
+        else
+            return Object.class;
+    }
 
-	public boolean isSingleton() {
-		return false;
-	}
+    public boolean isSingleton() {
+        return false;
+    }
 
-	public Object getProperty() {
-		return property;
-	}
+    public Object getProperty() {
+        return property;
+    }
 
-	public void setProperty(Object property) {
-		String temp=String.valueOf(property);
-		try {
-			temp = SecretUtil.decrypt(String.valueOf(property), null);
-		} catch (Exception e) {
-		}
-		if (CommonUtil.objectIsNotNull(temp)){
-			this.property=temp;
-		}else{
-			this.property=String.valueOf(property);
-		}
-	}
+    public void setProperty(Object property) {
+        String temp = String.valueOf(property);
+        try {
+            temp = SecretUtil.decrypt(String.valueOf(property), null);
+        } catch (Exception e) {
+        }
+        if (CommonUtil.objectIsNotNull(temp)) {
+            this.property = temp;
+        } else {
+            this.property = String.valueOf(property);
+        }
+    }
 }
